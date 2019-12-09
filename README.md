47c4a151bba926d16ae553b0e898196d9c3f993f
M3GA-Kun <abhikjain360@gmail.com>
2019-12-09 21:26:41 +0530

    2019 Final

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 This is a simple word predictor. Run words_predict.py using a Python 3.6 or above to use it.
 
-## words.db
-The database which has all words arranged in decreasing frequency of their usage, along with whether they are nouns, or adjectives. This is build using sqlite3 as it can easily be used in python.
+## The Database
+The database which has all words arranged in decreasing frequency of their usage, along with whether they are nouns, or adjectives. This is build using sqlite3 as it can easily be used in python, and is in words.db.
+
+The database has been taken in .csv form from https://www.kaggle.com/rtatman/english-word-frequency and is distributed under MIT license. The data was then converted to a table in the database by simply using csv and sqlite3 libraries available as standard libraries in python. This dataset includes 333,333 most commonly used single words on the English language web. 
+
+Then used http://www.ashley-bovan.co.uk/words/partsofspeech.html to get txt list of nouns and adjectives to sort words accordingly. 
+
+The table in the database is named as unigram_freq. it has 5 columns: sno, text, count, noun & adj. Columns count countains the number of times word occured in the text used to genereate the dataset, sno is simply the index of words and noun & adj are columns which countain 0 or 1 corresponding to whether the word in a noun or adjective
+
+Though the fact that we have words sorted in nouns and adjectives (and maybe add verbs in future), I don't use it to make better predictions of the text, yet, as it wil involve more complex algorithm, and maybe even machine learning.
+
+In future I might also use index of the predictions to get faster predictions, and also remove the one character lag while giving predictions. I also plan to add punctuations in the prediction.
 
 ## word_predict.py
-The python file which has all the code
+The python file which has all the code. It uses tkinter to render it's GUI and sqlite3 library to read from words.db
--- word_predict.py
+++ word_predict.py
@@ -7,33 +7,15 @@ import sqlite3
 from tkinter import *
 import time
 
-#establishing connection with database
-conn = sqlite3.connect('words.db')
-
-#setting a cursor to execute database queries
-cursor = conn.cursor()
-
-#main GUI window
-mainWindow = Tk(screenName=":0")
-mainWindow.geometry("700x430")
-mainWindow.configure(bg="#151515")
-
-#configuring columns for the grif
-mainWindow.grid_columnconfigure(0, weight=1)
-mainWindow.grid_columnconfigure(1, weight=1)
-mainWindow.grid_columnconfigure(2, weight=1)
-
-#viewer text: to display text
-viewer = Text(mainWindow, bg="white", bd=0, font="monospaced 14", width=40, height=21)
-viewer.grid(row=0, column=1, rowspan=2, pady=10)
+# function to clear the text, entry and listbox when button is pressed
 
 def clearViewer():
     viewer.delete(1.0, END)
-    predictions.delete(0,END)
+    predictions.delete(0, END)
     word.delete(0, END)
 
-clrBtn = Button(mainWindow, text="CLEAR", fg='red', command=clearViewer)
-clrBtn.grid(row=2, column=1)
+
+# The method executed when an item displayed on the listbox is selected
 
 def onSelect(event):
     widg = event.widget
@@ -41,31 +23,64 @@ def onSelect(event):
     value = str(widg.get(index)) + " "
     viewer.insert(END, value)
 
-#predictions listbox: to display predictions
-predictions = Listbox(mainWindow, bg="#636363", bd=0, font="verdana 14", height=18)
-predictions.grid(row=1, column=0, padx=10)
-predictions.bind("<<ListboxSelect>>", onSelect)
 
-#String variable to get input
-sv = StringVar()
+# method to make predictions and print them to listbox
 
-#method to make predictions and print them to listbox
 def getPredictions(s):
     cursor.execute(f"SELECT word FROM unigram_freq WHERE word LIKE '{s}%' ;")
     return cursor.fetchmany(15)
 
+
+# The method which is executed whenever the entry is updated
+# takes input from the entry and calls getPredictions to, well, get predictions
+
 def StartInput():
     w = sv.get()
     s = getPredictions(w)
-    predictions.delete(0,END)
+    predictions.delete(0, END)
     for i in s:
         predictions.insert(END, i[0])
     return True
 
-#word entry: to get input
-word = Entry(mainWindow, bg="#B3B3B3", bd=2, font="verdana 14", fg="black", relief=FLAT, width=20, 
-            textvariable=sv, validate="key", validatecommand=StartInput)
+# establishing connection with database
+conn = sqlite3.connect('words.db')
+
+# setting a cursor to execute database queries
+cursor = conn.cursor()
+
+# main GUI window
+mainWindow = Tk(screenName=":0")
+mainWindow.geometry("700x430")
+mainWindow.configure(bg="#151515")
+
+# configuring columns for the grid
+mainWindow.grid_columnconfigure(0, weight=1)
+mainWindow.grid_columnconfigure(1, weight=1)
+mainWindow.grid_columnconfigure(2, weight=1)
+
+# viewer text: to display text
+viewer = Text(mainWindow, bg="white", bd=0,
+              font="monospaced 14", width=40, height=21)
+viewer.grid(row=0, column=1, rowspan=2, pady=10)
+
+# the button to clear  the text, entry and listbox
+clrBtn = Button(mainWindow, text="CLEAR", fg='red', command=clearViewer)
+clrBtn.grid(row=2, column=1)
+
+# predictions listbox: to display predictions
+predictions = Listbox(mainWindow, bg="#636363", bd=0,
+                      font="verdana 14", height=18)
+predictions.grid(row=1, column=0, padx=10)
+predictions.bind("<<ListboxSelect>>", onSelect)
+
+# String variable to get input from Entry constantly, as soon as it's content is updated
+sv = StringVar()
+
+
+# word entry: to get input
+word = Entry(mainWindow, bg="#B3B3B3", bd=2, font="verdana 14", fg="black", relief=FLAT, width=20,
+             textvariable=sv, validate="key", validatecommand=StartInput)
 word.grid(row=0, column=0, padx=10, pady=10)
 
-#infinite loop for mainWindow untill stopped by the user
+# infinite loop for mainWindow untill stopped by the user (or by error, but let's hope that's not the case)
 mainWindow.mainloop()
